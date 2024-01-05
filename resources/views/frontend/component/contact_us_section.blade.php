050848748bd801620601755ae211380eec4012c4
Mohammad suhail <skhan282290@gmail.com>
2024-01-05 16:15:51 +0530

    some data

--- resources/views/frontend/component/contact_us_section.blade.php
+++ resources/views/frontend/component/contact_us_section.blade.php
@@ -2,63 +2,51 @@
 <section class="contact">
     <div class="container">
         <div class="row">
-           <div class="col-lg-6">
-           <h3 class="mb-3" data-aos="fade-up" data-aos-once="true">
-           Connect with our  
-                <span style="color: #E13333">legal experts</span> at
-             </h3>
+            <div class="col-lg-6 mb-lg-0 mb-5">
+                <h3 class="mb-3" data-aos="fade-up" data-aos-once="true">
+                    Connect with our
+                    <span style="color: #E13333">legal experts</span> at
+                </h3>
 
-             <h2 class="mb-md-3 mb-0 " data-aos="fade-up" data-aos-once="true">
-             an affordable rate of just 
-             <br>
-                <span style="color: #E13333">INR 899/-</span>
-             </h2>
-             <h4 class="" >
-             and embark on the journey to swiftly resolve <br> your legal matters
-             </h4>
+                <h2 class="mb-md-3 mb-0 " data-aos="fade-up" data-aos-once="true">
+                    an affordable rate of just
+                    <br>
+                    <span style="color: #E13333">INR 899/-</span>
+                </h2>
+                <h4 class="">
+                    and embark on the journey to swiftly resolve <br> your legal matters
+                </h4>
 
-             <div class="contact_icon_box">
-              <div class="row">
-                <div class="col-md-6 mt-3">
-                  <div
-                    class="contact_icon_container d-flex align-items-center justify-content-center flex-column gap-4 text-center"
-                    data-aos="fade-up"
-                    data-aos-once="true"
-                  >
-                    <img
-                      src="assets/frontend/images/loaction.png"
-                      class="contact_icon"
-                      alt="Contact Icon"
-                    />
-                    <p class="contact_title">Delhi</p>
-                    <p class="contact_address">
-                      
-                       Plot No. 66, First Floor, Okhla Industrial Estate, #TheHub Okhla
-                       Phase III New Delhi India.
-              
-                    </p>
-                  </div>
-                </div>
-                <div class="col-md-6 mt-3">
-                  <div
-                    class="contact_icon_container d-flex align-items-center justify-content-center flex-column gap-4 text-center"
-                    data-aos="fade-up"
-                    data-aos-once="true"
-                  >
-                    <img
-                      src="assets/frontend/images/loaction.png"
-                      class="contact_icon"
-                      alt="Contact Icon"
-                    />
-                    <p class="contact_title">24/7 Support</p>
-                    <p class="contact_address">
-                      +91 7428899959 <br />
-                      admin@seedlingassociates.com
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <!-- <div
+                <div class="contact_icon_box">
+                    <div class="row">
+                        <div class="col-md-6 mt-3">
+                            <div class="contact_icon_container d-flex align-items-center justify-content-center flex-column gap-4 text-center"
+                                data-aos="fade-up" data-aos-once="true">
+                                <img src="assets/frontend/images/loaction.png" class="contact_icon"
+                                    alt="Contact Icon" />
+                                <p class="contact_title">Delhi</p>
+                                <p class="contact_address">
+
+                                    Plot No. 66, First Floor, Okhla Industrial Estate, #TheHub Okhla
+                                    Phase III New Delhi India.
+
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mt-3">
+                            <div class="contact_icon_container d-flex align-items-center justify-content-center flex-column gap-4 text-center"
+                                data-aos="fade-up" data-aos-once="true">
+                                <img src="assets/frontend/images/loaction.png" class="contact_icon"
+                                    alt="Contact Icon" />
+                                <p class="contact_title">24/7 Support</p>
+                                <p class="contact_address">
+                                    +91 7428899959 <br />
+                                    admin@seedlingassociates.com
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div
                 class="contact_rating_container d-flex align-items-center gap-2 mt-5"
                 data-aos="fade-up"
                 data-aos-once="true"
@@ -70,9 +58,9 @@
                   <img src="images/stars.png" alt="" />
                 </div>
               </div> -->
-            </div>
+                </div>
 
-             <!-- <div class="">   
+                <!-- <div class="">   
                   <a href="/contact-us" contenteditable="false" style="cursor: pointer;"> 
                      <button class="payment_btn">
                      Make a Payment
@@ -85,7 +73,7 @@
                       <img src="/assets/frontend/images/call_white.png" alt=""><span class="fs-5">+91-7428899959</span>
                    </a>
                 </div> -->
-           </div>
+            </div>
             <div class="col-lg-6 mt-md-0 mt-5">
                 <div class="contact_form_container d-flex align-items-center justify-content-center">
                     @include('frontend.component.have_any_question_form')
